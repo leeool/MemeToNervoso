86283f5386ebdc1ba248e08f3709104833dab062
leeool <lgonsalez02@gmail.com>
2022-03-18 22:26:00 -0300

    finaliza o exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string frase;
+
+Console.WriteLine("O que voce acha de mim? Voce é muito sem graça\n");
+frase = Console.ReadLine()!;
+
+frase = frase.Remove(10);
+
+Console.WriteLine($"E sabe o que eu penso de voce?\n{frase}...\nNAO, PERA.\nTo nervoso");
